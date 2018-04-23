79428ed3052443611ac204357312ecaa5bb33ea0
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-23 17:14:12 +0200

    a1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+      String name = "szabi";
+      greet(name);
+
         System.out.println("Hello World!");
     }
+
+  private static void greet(String name) {
+    System.out.println("Hello " + name);
+  }
 }
